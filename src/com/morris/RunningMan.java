81cbb76d6f988c735977be955c53f1eeb1d53f76
acrus <austin430@gmail.com>
2019-03-10 16:36:00 +0800

    初始化java project

---
+++ src/com/morris/RunningMan.java
@@ -0,0 +1,15 @@
+package com.morris;
+
+public class RunningMan {
+
+	private String yoyo;
+
+	public String getYoyo() {
+		return yoyo;
+	}
+
+	public void setYoyo(String yoyo) {
+		this.yoyo = yoyo;
+	}
+	
+}
